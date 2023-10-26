989e3a44bba4645693f9edc0efce84e2bcfc3237
Nahuel Nuñez <nunezspinelli8@gmail.com>
2023-10-26 11:37:35 -0300

    test

---
+++ testrouting/src/app/layout.jsx
@@ -0,0 +1,29 @@
+export default function RootLayout({ children }) {
+    return (
+        <html lang="es">
+            <body>
+                <nav>
+                    <h1>Navbar</h1>
+
+                    <ul>
+                        <li>
+                            <a href="/">Home</a>
+                        </li>
+                        <li>
+                            <a href="/about">About</a>
+                        </li>
+                        <li>
+                            <a href="/carrito">Carrito</a>
+                        </li>
+                        <li>
+                            <a href="/app/productos">Productos</a>
+                        </li>
+
+                    </ul>
+                </nav>
+
+                {children}
+            </body>
+        </html>
+    )
+}
